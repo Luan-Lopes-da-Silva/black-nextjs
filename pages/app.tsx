ce3bb133cf2d4de16efdc2f3cfd5c32e1bbabb1d
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-07-19 07:54:28 -0300

    SSR

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../public/page.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({Component,pageProps}:AppProps){
 return <Component {...pageProps}/>  
---
+++ pages/dynamic.tsx
@@ -0,0 +1,52 @@
+import { GetServerSideProps,NextPage } from "next";
+import { ReactNode, useEffect, useState } from "react";
+import { Container,Row, Col } from "reactstrap";
+
+export const getServerSideProps: GetServerSideProps =async () => {
+const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res=>res.json())
+return{
+ props:{
+  serverSideData
+ } 
+} 
+} 
+type ApiResponse = {
+name:string,
+timestamp:Date  
+}
+const Dynamic:NextPage = (props:{
+childrem?:ReactNode
+serverSideData?:ApiResponse  
+}) => {
+const [clientSideData, setClientSideData] = useState<ApiResponse>() 
+
+useEffect(()=>{
+  fetchData()
+},[])
+
+const fetchData = async ()=>{
+  const data = await fetch('/api/hello').then(res=>res.json())
+  setClientSideData(data)
+}
+return(
+<Container tag='main'>
+<h1 className="my-5">
+Como funcionam as renderizações do Next.js  
+</h1>
+
+<Row>
+ <Col>
+ <h3>Gerado no servidor:</h3>
+ <h2>{props.serverSideData?.timestamp.toString()}</h2>
+ </Col>
+
+ <Col>
+ <h3>Gerado no cliente:</h3>
+ <h2>{clientSideData?.timestamp.toString()}</h2>
+ </Col>  
+</Row>
+</Container> 
+)
+}
+
+export default Dynamic
